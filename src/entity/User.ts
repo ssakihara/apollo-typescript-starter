d22ab060e4794e5b249dac1bf21cbc1f8fa5d9a8
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-04-22 15:03:02 +0900

    [update]setup

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Field, ObjectType, ID, Query, Resolver } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {
+@ObjectType()
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field((type) => ID)
   id: number;
 
   @Column()
+  @Field()
   firstName: string;
 
   @Column()
+  @Field()
   lastName: string;
 
   @Column()
+  @Field()
   age: number;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import 'reflect-metadata';
+import path from 'path';
 import { ApolloServer } from 'apollo-server-express';
-import * as express from 'express';
-import schema from 'schema';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 
-
 const main = async () => {
-  const server = new ApolloServer({ schema });
-
   await createConnection();
 
   const app = express();
 
+  const schema = await buildSchema({
+    resolvers: [path.resolve() + '/entity/**/*.ts'],
+  });
+
+  const server = new ApolloServer({ schema });
   server.applyMiddleware({ app });
 
-  app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+  app.listen({ port: process.env.PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+  );
 };
 
 main();
